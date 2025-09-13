55967e29eee70639f197dcceae6e1478d67e4630
whoeverxd <joshuamoises1995@gmail.com>
2025-09-13 00:58:57 -0400

    making requierd changes so I can run yarn build

--- src/components/header/category-navigation.ts
+++ src/components/header/category-navigation.ts
@@ -1,5 +1,23 @@
 import { customElement, bindable } from 'aurelia';
-import template from './category-navigation.html';
+
+const template = `<template>
+  <ul class="main-categories-list">
+    <li repeat.for="cat of categories" class="main-category-item">
+      <button class="main-category-btn" type="button" click.trigger="cat.isOpen = !cat.isOpen">
+        <span>\${cat.name}</span>
+        <svg class="main-category-arrow" aria-expanded.bind="cat.isOpen" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <polyline points="6 9 12 15 18 9" />
+        </svg>
+      </button>
+      <div if.bind="cat.isOpen" class="main-category-dropdown">
+        <div class="main-category-dropdown-content">
+          <!-- Aquí puedes renderizar subcategorías, links, etc. -->
+          <span class="dropdown-placeholder">Contenido de \${cat.name}</span>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>`;
 
 export interface Category {
   name: string;
