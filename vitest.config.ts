86e1d52f4fc443fdcb831c17b20929b1766896a8
whoeverxd <joshuamoises1995@gmail.com>
2025-09-13 01:29:20 -0400

    test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,14 @@
 import { fileURLToPath } from "node:url";
 import { mergeConfig, defineConfig } from "vitest/config";
 import viteConfig from "./vite.config";
+import { aurelia } from '@aurelia/vite-plugin';
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base: '/ChicksGold-FE-Challenge/', // para la publicación en GitHub Pages
+    plugins: [aurelia()]
+    ,
     test: {
       environment: "jsdom",
       watch: false,
